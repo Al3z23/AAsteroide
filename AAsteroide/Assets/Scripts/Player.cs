76758b756daaca6d37587d5ae7fe3ef077478753
Goldmoon7 <alejandradpm04@gmail.com>
2024-09-14 20:57:51 +0200

    sabado videos hasta 5
    Co-Authored-By: Al3z23 <125131489+Al3z23@users.noreply.github.com>

---
+++ AAsteroide/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float thrustForce = 100f;
+    public float rotationSpeed = 120f;
+
+    public GameObject gun, bulletPrefab;
+
+    private Rigidbody _rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotation = Input.GetAxis("Rotate") * Time.deltaTime;
+        float thrust = Input.GetAxis("Thrust") * Time.deltaTime * thrustForce;
+        Vector3 thrustDirection = transform.right;
+        _rigid.AddForce(thrustDirection * thrust);
+        transform.Rotate(Vector3.forward, -rotation + rotationSpeed);
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            GameObject bullet = Instantiate(bulletPrefab, gun.transform.position, Quaternion.identity);
+            Bullet balaScript = bullet.GetComponent<Bullet>();
+            balaScript.targetVector = transform.right;
+        }
+    }
+}
